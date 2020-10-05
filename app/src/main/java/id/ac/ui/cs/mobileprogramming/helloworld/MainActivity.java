277ecafc69cfea19ae44d679c696bb472ec0895c
vince10 <adi.vanbasten99@gmail.com>
2020-10-05 16:31:01 +0700

    add intent handler between home and stopwatch page back and forth

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -1,7 +1,9 @@
 package id.ac.ui.cs.mobileprogramming.helloworld;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,6 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     public int numberOfClicks = 0;
     public TextView text;
     public Button button, stopwatchButton;
@@ -46,4 +47,18 @@ public class MainActivity extends AppCompatActivity {
         this.startActivity(intent);
 
     }
-}
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit App")
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("NO",null).show();
+        }
+    }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/StopWatchActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/StopWatchActivity.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.mobileprogramming.helloworld;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
@@ -10,10 +11,30 @@ import android.widget.TextView;
 //init week 3
 public class StopWatchActivity extends AppCompatActivity {
 
+    public Button exitApp, home;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        exitApp = (Button) findViewById(R.id.exitApp);
+        home  = (Button) findViewById(R.id.homeButton);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleOnClickHome(v);
+            }
+        });
+
+
     }
+
+    public void handleOnClickHome( View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+
+    }
+
+
 
 }
