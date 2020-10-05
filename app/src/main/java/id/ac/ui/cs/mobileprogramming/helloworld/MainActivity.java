ddf03ae813d2861b80b125b1b9f03f7cd824f41f
vince10 <adi.vanbasten99@gmail.com>
2020-10-05 16:16:45 +0700

    add design for stopwatch

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.mobileprogramming.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +12,21 @@ public class MainActivity extends AppCompatActivity {
 
     public int numberOfClicks = 0;
     public TextView text;
-    public Button button;
+    public Button button, stopwatchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button1);
+        stopwatchButton = (Button) findViewById(R.id.stopwatchButton);
+        stopwatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleOnClickStopwatch(v);
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,5 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void handleOnClick (View view){
         numberOfClicks++;
+    }
+
+    public void handleOnClickStopwatch (View view) {
+        Intent intent = new Intent(this, StopWatchActivity.class);
+        this.startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/StopWatchActivity.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.mobileprogramming.helloworld;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class StopWatchActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+}
