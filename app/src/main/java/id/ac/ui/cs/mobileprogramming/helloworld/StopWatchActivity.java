116e0f1567b484b8dcc0430408f5ceedf3f1ad3b
vince10 <adi.vanbasten99@gmail.com>
2020-10-05 16:17:29 +0700

    init week 3

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/StopWatchActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/StopWatchActivity.java
@@ -7,6 +7,7 @@ import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+//init week 3
 public class StopWatchActivity extends AppCompatActivity {
 
     @Override
